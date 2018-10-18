8eadff91705a61a6e4c98993f5ef662c2dfeb06b
Nathaniel Flory <nathanielflory@gmail.com>
2018-10-18 00:22:19 -0400

    Added Google Maps Dependency and component -Added google-maps-react dependency -Added GoogleMaps folder and Maps.js file to HoursLocation component -Built out maps component -Added google maps API key -Basic styling of component (WIP) -Some minor style adjustments to OurOffice page

---
+++ oviedopremiere/src/Components/HoursLocation/GoogleMap/Map.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+const style = {
+    width: '48vw',
+    height: '45vh'
+  }
+
+class GoogleMap extends React.Component {
+    render() {
+        return (
+            <div>
+                <Map google={this.props.google} zoom={18} initialCenter={{ lat: 28.656574, lng: -81.183598 }} style={style}>
+                    <Marker onClick={this.onMarkerClick} name={'Current location'} />
+                </Map>
+            </div>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyB_0VyeaGxHK3wAKbO93miDKiBhJ2jJfcc')
+})(GoogleMap)
--- oviedopremiere/src/Components/HoursLocation/HoursLocation.js
+++ oviedopremiere/src/Components/HoursLocation/HoursLocation.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import HoursTable from './HoursTable/Table.js';
+import GoogleMap from './GoogleMap/Map.js';
 import './HoursLocation.css';
 
 class HoursLocation extends React.Component {
     render() {
         return (
             <div>
-            <div className='container'>
-                <div className='hours'>
-                    <h1>Hours of Operation</h1>
-                    <div className='table'>
-                    <HoursTable />
+                <div className='box'>
+                    <div className='hours'>
+                        <h1>Hours of Operation</h1>
+                        <div className='table'>
+                            <HoursTable />
+                        </div>
+                        <p>*Closed major holidays</p>
                     </div>
-                    <p>*Closed major holidays</p>
+                    <div className='location'>
+                        <h1>Location</h1>
+                        <p>We are located at 1445 E. Mitchell Hammock Rd., Oviedo, FL 32765. In order to get to the office you must turn onto Eastbridge Dr. and then take the first right as though you are going to the Fairwinds or StarChild. You will see our sign straight ahead and take the second right. We look foward to seeing you soon.</p>
+                        <GoogleMap />
+                    </div>
                 </div>
-                <div className='location'>
-                    <h1>Location</h1>
-                    <p>We are located at 1445 E. Mitchell Hammock Rd., Oviedo, FL 32765. In order to get to the office you must turn onto Eastbridge Dr. and then take the first right as though you are going to the Fairwinds or StarChild. You will see our sign straight ahead and take the second right. We look foward to seeing you soon.</p>
-                </div>
-            </div>
-            <h3>Call today to schedule your appointment <br/>(407) 977-6464</h3>
+                <h3>Call today to schedule your appointment <br />(407) 977-6464</h3>
             </div>
         );
     }
